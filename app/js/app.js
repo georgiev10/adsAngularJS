ba8ffa48b989fde971b88e7584b1fe074d55ab90
georgiev10 <georgiev10@abv.bg>
2015-01-08 17:30:02 +0200

    Add paging for ads
    Add paging for ads

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var app = angular.module('app', ['ngRoute', 'ngResource']);
+var app = angular.module('app', ['ngRoute', 'ngResource', 'ui.bootstrap.pagination']);
 
 app.constant('baseServiceUrl', 'http://softuni-ads.azurewebsites.net');
 app.constant('pageSize', 2);
@@ -27,3 +27,5 @@ app.config(function ($routeProvider) {
     );
 
 });
+
+
