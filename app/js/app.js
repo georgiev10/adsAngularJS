224b92f6c2e6314ab7f129fd2a9ac633a6aa68cb
georgiev10 <georgiev10@abv.bg>
2015-01-09 03:26:04 +0200

    Fix page number
    Fix page number

--- app/js/app.js
+++ app/js/app.js
@@ -3,7 +3,7 @@
 var app = angular.module('app', ['ngRoute', 'ngResource', 'ui.bootstrap.pagination']);
 
 app.constant('baseServiceUrl', 'http://softuni-ads.azurewebsites.net');
-app.constant('pageSize', 6);
+app.constant('pageSize', 10);
 
 app.config(function ($routeProvider) {
 
